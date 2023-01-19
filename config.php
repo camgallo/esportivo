8dcdd1d1af953ef5b3c0f3da4d73d7297a35a0e7
cgallo <camgallo@gmail.com>
2023-01-18 23:43:50 -0300

    update port

--- config.php
+++ config.php
@@ -4,7 +4,8 @@ $host = "containers-us-west-50.railway.app";
 $user = "root";
 $pass = "tn9DLLJhi2mXyIfmUdMu";
 $banco = "railway";
-$conexao = new mysqli($host,$user,$pass,$banco);
+$port = "6663"
+$conexao = new mysqli($host,$user,$pass,$banco,$port);
 
 if (mysqli_connect_errno()) {
     die('Não foi possível conectar-se ao banco de dados: ' . mysqli_connect_error());
